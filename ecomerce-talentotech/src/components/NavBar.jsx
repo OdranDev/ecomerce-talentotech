9844498f9ad4a3e55b4b3cb27d5f74ae0b8a8da2
odran21 <jleonardoguevaram@gmail.com>
2025-05-27 14:20:34 -0500

    Agregado componente NavBar con estilos Sass

---
+++ ecomerce-talentotech/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+// src/components/NavBar.jsx
+import '../styles/NavBar.sass'
+
+function NavBar() {
+  return (
+    <header>
+        <nav className="navbar">
+        <div className="navbar__logo">TalentoTech Shop</div>
+        <ul className="navbar__links">
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/productos">Productos</a></li>
+            <li><a href="/carrito">Carrito</a></li>
+        </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default NavBar;
